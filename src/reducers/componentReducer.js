bc567ff0a8746b7bd428beb1a3ce6f96e7780b07
Leo Dalbert <leo@dalbert.us>
2020-10-18 10:19:52 -1000

    fixed error from empty component atribute objects

--- src/reducers/componentReducer.js
+++ src/reducers/componentReducer.js
@@ -21,7 +21,7 @@ const initialState = {
     installation_date: '',
     warranty_start_date: '',
     space_name: '',
-    pace_number: '',
+    space_number: '',
     floor_name: '',
     floor_number: null,
     category: '',
@@ -73,62 +73,75 @@ export default function (state = initialState, action) {
         id,
         name,
         instance_name,
-        description: instance_description,
+        instance_description,
         barcode,
         serial_number,
         installation_date,
         warranty_start_date,
-        attributes: instance_attributes,
-        space: [
-          {
-            space: {
-              name: space_name,
-              number: space_number,
-              floor: { name: floor_name, number: floor_number },
-            },
-          },
-        ],
-        component_type: {
-          category,
-          name: type_name,
-          description: type_description,
-          labour_warranty_duration,
-          labour_warranty_guarantor,
-          parts_warranty_guarantor,
-          parts_warranty_duration,
-          warranty_duration_unit,
-          manufacturer,
-          model_number,
-          attributes: type_attributes,
-        },
-      } = payload;
+        space_name,
+        space_number,
+        floor_name,
+        floor_number,
+        category,
+        type_name,
+        type_description,
+        labour_warranty_duration,
+        labour_warranty_guarantor,
+        parts_warranty_guarantor,
+        parts_warranty_duration,
+        warranty_duration_unit,
+        manufacturer,
+        model_number,
+        type_attributes,
+        instance_attributes,
+      } = initialState.dialogComponent;
+      let data = payload;
+      if (!data.component_type) {
+        data.component_type = {};
+      }
+      if (!data.space[0]) {
+        data.space[0] = {};
+      }
+      if (!data.space[0].floor) {
+        data.space[0].floor = {};
+      }
       return {
         ...state,
         dialogComponent: {
-          id,
-          name,
-          instance_name,
-          instance_description,
-          barcode,
-          serial_number,
-          installation_date,
-          warranty_start_date,
-          space_name,
-          space_number,
-          floor_name,
-          floor_number,
-          category,
-          type_name,
-          type_description,
-          labour_warranty_duration,
-          labour_warranty_guarantor,
-          parts_warranty_guarantor,
-          parts_warranty_duration,
-          warranty_duration_unit,
-          manufacturer,
-          model_number,
-          instance_attributes,
-          type_attributes,
+          id: data.id || id,
+          name: data.name || name,
+          instance_name: data.instance_name || instance_name,
+          instance_description: data.description || instance_description,
+          barcode: data.barcode || barcode,
+          serial_number: data.serial_number || serial_number,
+          installation_date: data.installation_date || installation_date,
+          warranty_start_date: data.warranty_start_date || warranty_start_date,
+          space_name: data.space[0].space.name || space_name,
+          space_number: data.space[0].space.number || space_number,
+          floor_name: data.space[0].space.floor.name || floor_name,
+          floor_number: data.space[0].space.floor.number || floor_number,
+          category: data.component_type.category || category,
+          type_name: data.component_type.name || type_name,
+          type_description: data.component_type.description || type_description,
+          labour_warranty_duration:
+            data.component_type.labour_warranty_duration ||
+            labour_warranty_duration,
+          labour_warranty_guarantor:
+            data.component_type.labour_warranty_guarantor ||
+            labour_warranty_guarantor,
+          parts_warranty_guarantor:
+            data.component_type.parts_warranty_guarantor ||
+            parts_warranty_guarantor,
+          parts_warranty_duration:
+            data.component_type.parts_warranty_duration ||
+            parts_warranty_duration,
+          warranty_duration_unit:
+            data.component_type.warranty_duration_unit ||
+            warranty_duration_unit,
+          manufacturer: data.component_type.manufacturer || manufacturer,
+          model_number: data.component_type.model_number || model_number,
+          instance_attributes: data.attributes || instance_attributes,
+          type_attributes: data.component_type.attributes || type_attributes,
         },
       };
     case CLEAR_DIALOG_COMPONENT:
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import dayjs from 'dayjs';
 
 // function returns priority level string from number
 export const priority = (num) => {
@@ -23,6 +24,103 @@ export const insertBreak = (arr) => {
     }
     return result;
   }, []);
+};
+
+// function to generate list of fields workorder page
+
+export const workOrderFieldGen = (workOrder) => {
+  const {
+    request_number,
+    request_date,
+    assigned_priority,
+    request_email,
+    submitted_by,
+    building,
+    floor,
+    space,
+    request_description,
+    location_description,
+    request_telephone,
+    due_date,
+    administrator_to_technician_comment,
+  } = workOrder;
+
+  let FIELDS = [];
+  request_number &&
+    FIELDS.push({ lable: 'Request Number:', detail: request_number });
+  request_date &&
+    FIELDS.push({
+      lable: 'Request Date:',
+      detail: dayjs(request_date).format('MM/DD/YYYY h:m A'),
+    });
+  assigned_priority &&
+    FIELDS.push({
+      lable: 'Priority:',
+      detail: priority(assigned_priority),
+      detailColor: assigned_priority === 1 && 'redDetail',
+    });
+  due_date &&
+    FIELDS.push({
+      lable: 'Due Date:',
+      detail: dayjs(due_date).format('MM/DD/YYYY'),
+      detailColor: 'redDetail',
+    });
+
+  request_email &&
+    FIELDS.push({
+      lable: 'Submited By:',
+      detail: insertBreak([
+        <a
+          key={request_email}
+          style={{ textDecoration: 'none' }}
+          href={
+            'mailto:' +
+            request_email +
+            `?subject=Workorder request for room ${space.number} in ${building.name}`
+          }
+        >{`${submitted_by} (${request_email})`}</a>,
+        request_telephone && (
+          <a
+            key={request_telephone}
+            style={{ textDecoration: 'none' }}
+            href={'tel:' + request_telephone}
+          >
+            {request_telephone}
+          </a>
+        ),
+      ]),
+    });
+
+  (building.number || building.name) &&
+    FIELDS.push({
+      lable: 'Location:',
+      detail: [
+        `${building.number} ${(building.number || building.name) && '- '}${
+          building.name
+        }`,
+        <br key='1' />,
+        floor && !!floor.name && floor.name,
+        floor && !!floor.name && <br key='2' />,
+        `${space && space.number && space.number} ${
+          space && space.number && space.name && '- '
+        }${space && space.name && space.name}`,
+        location_description && <br key='3' />,
+        location_description && location_description,
+      ],
+    });
+  request_description &&
+    FIELDS.push({
+      lable: 'Request Description:',
+      detail: insertBreak(request_description.split('\r\n')),
+    });
+
+  administrator_to_technician_comment &&
+    FIELDS.push({
+      lable: 'Administrator Comments:',
+      detail: administrator_to_technician_comment,
+    });
+
+  return FIELDS;
 };
 
 // function to generate list of fields for component modal
